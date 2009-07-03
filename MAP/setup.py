e6b5148fd1993a52165c194bfd80fb6733d9f1af
poinot <poinot@localhost.localdomain>
2009-07-03 18:31:49 +0200

    move pyCGNSconfig

--- MAP/setup.py
+++ MAP/setup.py
@@ -4,25 +4,41 @@
 # See license file in the root directory of this Python module source      
 # -------------------------------------------------------------------------
 from  distutils.core import setup, Extension
-import pyCGNSconfig
 
-include_dirs=[\
-    '/home/tools/local/x86_64/include',\
-    '/home/tools/local/x86_64/lib/python2.5/site-packages/numpy/core/include'\
-]
+# --- pyConfig search
+import os
+import sys
+spath=sys.path[:]
+sys.path=[os.getcwd(),'%s/..'%(os.getcwd())]
+try:
+  import pyCGNSconfig
+except ImportError:
+  print 'pyGCNS[ERROR]: MAP cannot find pyCGNSconfig.py file!'
+  sys.exit(1)
+sys.path=spath
+# ---
 
-library_dirs=['/home/tools/local/x86_64/lib']
-optional_libs=['cgns','hdf5']
+try:
+  if (not pyCGNSconfig.HAS_HDF5):
+    print 'pyGCNS[ERROR]: MAP requires HDF5, please set pyCGNSconfig.py file!'
+    sys.exit(1)
+  cf_include_dirs=pyCGNSconfig.include_dirs
+  cf_library_dirs=pyCGNSconfig.library_dirs
+  cf_optional_libs=pyCGNSconfig.optional_libs
+except:
+  print 'pyGCNS[ERROR]: bad pyCGNSconfig.py file for MAP!'
+  sys.exit(1)
 
 setup (
   name         = "CGNS.MAP",
-  version      = "0.1",
+  version      = pyCGNSconfig.version,
   description  = "pyCGNS MAPping SIDS-to-Python",
   author       = "marc Poinot",
   author_email = "marc.poinot@onera.fr",
-  ext_modules  = [Extension('CGNS.MAP', 
-                           sources=['MAPmodule.c'],
-                           include_dirs = include_dirs,
-                           library_dirs = library_dirs,
-                           libraries    = optional_libs)],
+  ext_modules  = [Extension('CGNS.MAP',sources = ['MAPmodule.c'],
+                  include_dirs = cf_include_dirs,
+                  library_dirs = cf_library_dirs,
+                  libraries    = cf_optional_libs)],
 )
+
+# --- last line
--- pyCGNSconfig.py
+++ pyCGNSconfig.py
@@ -1,5 +1,10 @@
 
-majorversion="
+majorversion=4
+minorversion=0
+releaseversion=1
+
+version="v%d.%d.%d"
+
 include_dirs=[\
     '/home/tools/local/x86_64/include',\
     '/home/tools/local/x86_64/lib/python2.5/site-packages/numpy/core/include'\
@@ -7,3 +12,5 @@ include_dirs=[\
 
 library_dirs=['/home/tools/local/x86_64/lib']
 optional_libs=['cgns','hdf5']
+
+HAS_HDF5=1
