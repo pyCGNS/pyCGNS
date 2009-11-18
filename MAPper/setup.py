fb8984e3641e0a6ca0043a8961429c4b65a312d4
Marc Poinot <marc.poinot@onera.fr>
2009-11-18 16:16:00 +0100

    test kw

--- MAPper/setup.py
+++ MAPper/setup.py
@@ -2,7 +2,7 @@
 # pyCGNS.MAP - CFD General Notation System - SIDS-to-Python MAPping            
 # See license.txt file in the root directory of this Python module source  
 # -------------------------------------------------------------------------
-# $pyCGNS$ $CGNS.MAP$ $Last$
+# $pyCGNS$ $pyCGNS.MAP$ $Last$
 # -------------------------------------------------------------------------
 import os
 
