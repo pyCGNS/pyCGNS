a95ab9d55b30d4b15ede6752f11ec9c1c806a4e3
Marc Poinot <marc.poinot@onera.fr>
2010-11-10 14:41:17 +0100

    remove XML based VAL

--- VALidater/CGNS/VAL/tools/__init__.py
+++
@@ -1,9 +0,0 @@
-#  -------------------------------------------------------------------------
-#  pyCGNS.VAL - Python package for CFD General Notation System - VALidater
-#  See license.txt file in the root directory of this Python module source  
-#  -------------------------------------------------------------------------
-#  $Release$
-#  -------------------------------------------------------------------------
-#
-#
-pass
