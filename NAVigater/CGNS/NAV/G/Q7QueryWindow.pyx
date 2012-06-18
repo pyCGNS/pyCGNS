fae0d69c646f670a69dc3e0c4944b7748cbfa4f4
Marc Poinot <marc.poinot@onera.fr>
2012-06-18 16:37:04 +0200

    none

--- NAVigater/CGNS/NAV/G/Q7QueryWindow.pyx
+++ NAVigater/CGNS/NAV/G/Q7QueryWindow.pyx
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'CGNS/NAV/T/Q7QueryWindow.ui'
 #
-# Created: Fri Jun  8 10:47:04 2012
-#      by: pyside-uic 0.2.13 running on PySide 1.0.9
+# Created: Mon Jun 18 16:25:06 2012
+#      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
 
