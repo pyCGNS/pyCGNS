4ac868cc7e82ece8bcc3d10225e3b9d8cc34c906
Marc Poinot <marc.poinot@onera.fr>
2013-07-11 16:00:00 +0200

    prod update

--- lib/pyCGNSconfig_default.py
+++ lib/pyCGNSconfig_default.py
@@ -75,7 +75,7 @@ PFX='### pyCGNS:'
 #
 __version__=4 # @@UPDATEVERSION@@
 __release__=4 # @@UPDATERELEASE@@
-__revision__=440 # @@UPDATEREVISION@@
+__revision__=442 # @@UPDATEREVISION@@
 __vid__="%s.%s.%s"%(__version__,__release__,__revision__)
 __doc__="""pyCGNS - %s - Python package for CFD General Notation System"""\
         %(__vid__)
@@ -148,7 +148,7 @@ NUMPY_EXTRA_ARGS=%(NUMPY_EXTRA_ARGS)s
 
 __version__=4 # @@UPDATEVERSION@@
 __release__=4 # @@UPDATERELEASE@@
-__revision__=440 # @@UPDATEREVISION@@
+__revision__=442 # @@UPDATEREVISION@@
 __vid__="%%s.%%s.%%s"%%(__version__,__release__,__revision__)
 __doc__='pyCGNS - %%s - Python package for CFD General Notation System'\
         %%(__vid__)
