2ad0e4e584a9fb45b8e915323b28471242f1392c
guillaume.turpin <devnull@localhost>
2013-07-29 15:30:18 +0200

    [VAL] tests on BC and BCDataSet -> surface patch only

--- VALidater/CGNS/VAL/suite/SIDS/07zonesubregion.py
+++ VALidater/CGNS/VAL/suite/SIDS/07zonesubregion.py
@@ -13,7 +13,7 @@ TESTS=[]
 #  -------------------------------------------------------------------------
 tag='zonesubregion'
 diag=True
-def makeCorrectTree(vertexsize,cellsize):
+def makeCorrectTree(vertexsize,cellsize,ntris):
   T=CGL.newCGNSTree()
   b=CGL.newBase(T,'Base',3,3)
   s=NPY.array([[vertexsize,cellsize,0]],dtype='i',order='F')
@@ -23,13 +23,15 @@ def makeCorrectTree(vertexsize,cellsize):
   d=CGL.newDataArray(g,CGK.CoordinateY_s,NPY.ones((vertexsize),dtype='d',order='F'))
   d=CGL.newDataArray(g,CGK.CoordinateZ_s,NPY.ones((vertexsize),dtype='d',order='F'))
   tetras=CGL.newElements(z,'TETRAS',CGK.TETRA_4_s,NPY.ones((cellsize*4),dtype='i'),NPY.array([[1,cellsize]],'i',order='F'))
+  tris=CGL.newElements(z,'TRIS',CGK.TRI_3_s,NPY.ones((ntris*3),dtype='i'),NPY.array([[cellsize+1,cellsize+ntris]],'i',order='F'))
   zbc=CGL.newZoneBC(z)
-  n=CGL.newBoundary(zbc,'BC',[range(1,cellsize+1)],btype=CGK.Null_s,family=None,pttype=CGK.PointList_s)
-  g=CGL.newGridLocation(n,value=CGK.CellCenter_s)  
+  n=CGL.newBoundary(zbc,'BC',[range(cellsize+1,cellsize+ntris+1)],btype=CGK.Null_s,family=None,pttype=CGK.PointList_s)
+  g=CGL.newGridLocation(n,value=CGK.FaceCenter_s)  
   return (T,b,z)
 vertexsize = 20
 cellsize   = 7
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
+ntris      = 11
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr1=CGU.newNode('SubRegion1',NPY.array([3],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointRange_s,NPY.array([[1,vertexsize]],dtype=NPY.int32,order='Fortran'),[],CGK.IndexRange_ts,parent=zsr1)
 CGL.newDataArray(zsr1,'var',value=NPY.ones(vertexsize,dtype='d',order='Fortran'))
@@ -38,7 +40,7 @@ CGU.newNode(CGK.PointList_s,NPY.array([range(1,vertexsize+1)],dtype=NPY.int32,or
 CGL.newDataArray(zsr2,'var',value=NPY.ones(vertexsize,dtype='d',order='Fortran'))
 zsr3=CGU.newNode('SubRegion3',NPY.array([2],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.BCRegionName_s,CGU.setStringAsArray('BC'),[],CGK.Descriptor_ts,parent=zsr3)
-CGL.newDataArray(zsr3,'var',value=NPY.ones(cellsize,dtype='d',order='Fortran'))
+CGL.newDataArray(zsr3,'var',value=NPY.ones(ntris,dtype='d',order='Fortran'))
 zsr5=CGU.newNode('SubRegion5',NPY.array([3],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointRange_s,NPY.array([[1,cellsize]],dtype=NPY.int32,order='Fortran'),[],CGK.IndexRange_ts,parent=zsr5)
 CGL.newGridLocation(zsr5,value=CGK.CellCenter_s)
@@ -52,7 +54,7 @@ TESTS.append((tag,T,diag))
 #  -------------------------------------------------------------------------
 tag='zonesubregion bad node value'
 diag=False
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr1=CGU.newNode('SubRegion1',NPY.array([4],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointRange_s,NPY.array([[1,vertexsize]],dtype=NPY.int32,order='Fortran'),[],CGK.IndexRange_ts,parent=zsr1)
 TESTS.append((tag,T,diag))
@@ -60,7 +62,7 @@ TESTS.append((tag,T,diag))
 #  -------------------------------------------------------------------------
 tag='zonesubregion bad Grid Location'
 diag=False
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr1=CGU.newNode('SubRegion1',NPY.array([2],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointRange_s,NPY.array([[1,cellsize]],dtype=NPY.int32,order='Fortran'),[],CGK.IndexRange_ts,parent=zsr1)
 CGL.newGridLocation(zsr1,value=CGK.CellCenter_s)
@@ -69,7 +71,7 @@ TESTS.append((tag,T,diag))
 #  -------------------------------------------------------------------------
 tag='zonesubregion bad children'
 diag=False
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr1=CGU.newNode('SubRegion1',NPY.array([3],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointRange_s,NPY.array([[1,vertexsize]],dtype=NPY.int32,order='Fortran'),[],CGK.IndexRange_ts,parent=zsr1)
 CGU.newNode(CGK.PointList_s,NPY.array([range(1,vertexsize+1)],dtype=NPY.int32,order='Fortran'),[],CGK.IndexArray_ts,parent=zsr1)
@@ -79,7 +81,7 @@ TESTS.append((tag,T,diag))
 #  -------------------------------------------------------------------------
 tag='zonesubregion bad regionname'
 diag=False
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr1=CGU.newNode('SubRegion1',NPY.array([2],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.BCRegionName_s,CGU.setStringAsArray('bc'),[],CGK.Descriptor_ts,parent=zsr1)
 zsr4=CGU.newNode('SubRegion4',NPY.array([2],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
@@ -89,7 +91,7 @@ TESTS.append((tag,T,diag))
 #  -------------------------------------------------------------------------
 tag='zonesubregion bad dataarray shapes'
 diag=False
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr1=CGU.newNode('SubRegion1',NPY.array([3],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointRange_s,NPY.array([[1,vertexsize]],dtype=NPY.int32,order='Fortran'),[],CGK.IndexRange_ts,parent=zsr1)
 CGL.newDataArray(zsr1,'var',value=NPY.ones(vertexsize+1,dtype='d',order='Fortran'))
@@ -98,15 +100,13 @@ CGU.newNode(CGK.PointList_s,NPY.array([range(1,vertexsize+1)],dtype=NPY.int32,or
 CGL.newDataArray(zsr2,'var',value=NPY.ones(vertexsize+1,dtype='d',order='Fortran'))
 zsr3=CGU.newNode('SubRegion3',NPY.array([2],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.BCRegionName_s,CGU.setStringAsArray('BC'),[],CGK.Descriptor_ts,parent=zsr3)
-CGL.newDataArray(zsr3,'var',value=NPY.ones(cellsize+1,dtype='d',order='Fortran'))
+CGL.newDataArray(zsr3,'var',value=NPY.ones(cellsize,dtype='d',order='Fortran'))
 TESTS.append((tag,T,diag))
 
 #  -------------------------------------------------------------------------
 tag='zonesubregion bad point list on cell and face subregions'
 diag=False
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
-ntris = 12
-tris=CGL.newElements(z,'TRIS',CGK.TRI_3_s,NPY.ones((ntris*3),dtype='i'),NPY.array([[cellsize+1,cellsize+ntris]],'i',order='F'))
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr6=CGU.newNode('SubRegion6',NPY.array([3],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointList_s,NPY.array([range(1,cellsize+2)],dtype=NPY.int32,order='Fortran'),[],CGK.IndexArray_ts,parent=zsr6)
 CGL.newGridLocation(zsr6,value=CGK.CellCenter_s)
@@ -118,9 +118,7 @@ TESTS.append((tag,T,diag))
 #  -------------------------------------------------------------------------
 tag='zonesubregion bad point range on cell and face subregions'
 diag=False
-(T,b,z)=makeCorrectTree(vertexsize,cellsize)
-ntris = 12
-tris=CGL.newElements(z,'TRIS',CGK.TRI_3_s,NPY.ones((ntris*3),dtype='i'),NPY.array([[cellsize+1,cellsize+ntris]],'i',order='F'))
+(T,b,z)=makeCorrectTree(vertexsize,cellsize,ntris)
 zsr5=CGU.newNode('SubRegion5',NPY.array([3],dtype=NPY.int32,order='Fortran'),[],CGK.ZoneSubRegion_ts,parent=z)
 CGU.newNode(CGK.PointRange_s,NPY.array([[1,cellsize+1]],dtype=NPY.int32,order='Fortran'),[],CGK.IndexRange_ts,parent=zsr5)
 CGL.newGridLocation(zsr5,value=CGK.CellCenter_s)
