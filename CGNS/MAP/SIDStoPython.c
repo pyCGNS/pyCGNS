34c199d7583ade3616aca085ec624d3a21d3185d
MicK7 <mickey.phy@gmail.com>
2023-05-10 23:14:00 +0200

    Better handling of HDF5 defines to select API
    * update l3 to handle HDF5 API changes
    * update macro check in SIDStoPython.c

--- CGNS/MAP/SIDStoPython.c
+++ CGNS/MAP/SIDStoPython.c
@@ -211,6 +211,13 @@ extern void objlist_status(char *tag);
     printf( msg );\
   }}
 
+/* Handle HDF5 API variability */
+#if H5_VERSION_GE(1,12,0) && !defined(H5_USE_110_API) && !defined(H5_USE_18_API) && !defined(H5_USE_16_API)
+#define ADFH_HDF5_HAVE_112_API 1
+#else
+#define ADFH_HDF5_HAVE_112_API 0
+#endif
+
 /* ------------------------------------------------------------------------- */
 #ifdef CHLONE_TIMING
 rusage_snapshot(char *tag)
@@ -439,7 +446,7 @@ static L3_Cursor_t *s2p_addoneHDF(char* dirname, char *filename,
         free(fullpath);
         return NULL;
       }
-#if H5_VERSION_GE(1,12,0)
+#if ADFH_HDF5_HAVE_112_API
       ishdf = H5Fis_accessible(fullpath, H5P_DEFAULT);
 #else
       ishdf = H5Fis_hdf5(fullpath);
